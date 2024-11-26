32378b9ace0b8100e2af350d9daf767a107b74c4
Abrarulctg <jubairctg99@gmail.com>
2024-11-27 01:03:28 +0600

    Added sub heading for section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import OurRoom from "@/components/ourRoom/page";
+
 export default function Home() {
   return (
     <div className="container">
       <h1>Next.js + TypeScript + Tailwind CSS</h1>
+      <OurRoom />
     </div>
   );
 }
---
+++ components/ourRoom/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ourRoom.css';
+
+
+const OurRoom = () => {
+    return (
+        <div className='max-w-7xl mx-auto '>
+            <p className='sub-title text-center text-[#3E48A8]'>Our Rooms</p>
+            <h1 className='text-3xl font-bold text-center my-7'>Explore Our <span className='text-[#3E48A8]'>Rooms</span></h1>
+
+            {/* hotel Cards */}
+        </div>
+    );
+};
+
+export default OurRoom;
