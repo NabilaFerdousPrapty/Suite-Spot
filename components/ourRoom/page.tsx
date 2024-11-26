c9afa27aefbc8d5b2feee2b98e469f8208948177
Abrarulctg <jubairctg99@gmail.com>
2024-11-27 01:10:50 +0600

    Added demo card image

--- components/ourRoom/page.tsx
+++ components/ourRoom/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ourRoom.css';
-
+import Image from 'next/image';
+import hotel1 from '../../public/image/hotel1.jpeg';
 
 const OurRoom = () => {
     return (
@@ -9,6 +10,18 @@ const OurRoom = () => {
             <h1 className='text-3xl font-bold text-center my-7'>Explore Our <span className='text-[#3E48A8]'>Rooms</span></h1>
 
             {/* hotel Cards */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+                <div className='w-full border'>
+                    <Image
+                        src={hotel1}
+                        alt={`hotel image`}
+                        className="w-full"
+                    />
+                    <div className='p-4'>
+                        <h1>Hotel 1</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
