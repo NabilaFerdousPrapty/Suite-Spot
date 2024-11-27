767fdb6c93b0995f79154217fa4033281dd50d19
Abrarulctg <jubairctg99@gmail.com>
2024-11-27 11:49:50 +0600

    Added responsive modification on room section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import OurRoom from "@/components/ourRoom/page";
+import Rooms from "@/components/rooms/page";
+import Services from "@/components/services/page";
 
 export default function Home() {
   return (
     <div className="container">
       <h1>Next.js + TypeScript + Tailwind CSS</h1>
-      <OurRoom />
+      <Rooms />
+      <Services />
     </div>
   );
 }
---
+++ components/services/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            Explore Our Services
+        </div>
+    );
+};
+
+export default Services;
