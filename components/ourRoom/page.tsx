ce739091d68a3a8cf15ba951141973d8d611c505
Abrarulctg <jubairctg99@gmail.com>
2024-11-27 09:07:05 +0600

    Added rest of the card

--- components/ourRoom/page.tsx
+++ components/ourRoom/page.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './ourRoom.css';
 import Image from 'next/image';
 import hotel1 from '../../public/image/hotel1.jpeg';
+import { TbBathFilled } from 'react-icons/tb';
+import { FaBed } from 'react-icons/fa';
+import { FaWifi } from 'react-icons/fa6';
 
 const OurRoom = () => {
     return (
@@ -17,8 +20,21 @@ const OurRoom = () => {
                         alt={`hotel image`}
                         className="w-full"
                     />
-                    <div className='p-4'>
-                        <h1>Hotel 1</h1>
+                    <div className='p-4 space-y-4'>
+                        <h1 className='text-2xl font-bold'>Junio Suite</h1>
+                        <div className='text-gray-500 flex justify-between font-semibold
+                        '>
+                            <p className='flex items-center'><TbBathFilled className='text-[#3E48A8] mr-2' />
+                                2 Bath</p>
+                            <p className='flex items-center'><FaBed className='text-[#3E48A8] mr-2' />
+                                2 Beds</p>
+
+                            <p className='flex items-center'><FaWifi className='text-[#3E48A8] mr-2' />
+                                Wifi</p>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, voluptate!</p>
+                        <p className='text-xl font-bold'>$50 / Per Night</p>
+                        <button className='button w-full bg-[#3E48A8] text-white p-2'>Book Now</button>
                     </div>
                 </div>
             </div>
