64878630947caaa5e72c77fda80264027d6d27f2
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-11-28 20:52:16 +0600

    prapty responsive issue fixed

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -2,17 +2,19 @@ import { AiOutlineArrowRight } from "react-icons/ai"; // React icon for arrow ri
 
 const SignUpForm = () => {
   return (
-    <section className="max-w-6xl mx-auto rounded-xl shadow-2xl ">
+    <section className="max-w-6xl mx-auto rounded-xl shadow-2xl">
       <div className="flex justify-center min-h-screen">
+        {/* Left Image Section (hidden on small screens, visible on medium and larger screens) */}
         <div
-          className="hidden bg-cover lg:block lg:w-3/5 bg-center rounded-2xl"
+          className="hidden lg:block lg:w-3/5 bg-cover bg-center rounded-2xl"
           style={{
             backgroundImage:
               "url('https://i.ibb.co.com/HV7Gc8x/9f14737c-2be7-4b2f-80ed-d9c36556e7dd.gif')",
           }}
         ></div>
 
-        <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+        {/* Right Form Section */}
+        <div className="flex items-center w-full max-w-3xl p-6 mx-auto lg:px-12 lg:w-3/5">
           <div className="w-full">
             <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
               Get your free account now.
@@ -27,8 +29,6 @@ const SignUpForm = () => {
               <h1 className="text-gray-500 dark:text-gray-300">
                 Select type of account
               </h1>
-
-              <div className="mt-3 md:flex md:items-center md:-mx-2"></div>
             </div>
 
             <form className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
@@ -97,7 +97,8 @@ const SignUpForm = () => {
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-              <div className="col-span-2 ">
+
+              <div className="md:col-span-2">
                 <button
                   type="submit"
                   className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
@@ -107,6 +108,7 @@ const SignUpForm = () => {
                 </button>
               </div>
             </form>
+
             <p className="mt-6 text-sm text-center text-gray-400">
               Already have an account?{" "}
               <a
